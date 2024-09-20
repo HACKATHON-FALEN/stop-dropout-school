1afdbcfe0ad15fa485422c8b786639916eabd3ed
EnascutaRazvan <79245049+EnascutaRazvan@users.noreply.github.com>
2024-09-20 22:31:40 +0300

    feat: added profile jpg and background

--- src/components/Profile/views/Profile.js
+++ src/components/Profile/views/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import profilePicture from '../assets/img/team-2-800x800.jpg'
+import profilePicture from '../assets/img/profile_teacher.jpg'
 import backgroundPicture from '../assets/img/Background.jpg'
 
 
@@ -99,30 +99,21 @@ export default function Profile() {
                 </div>
                 <div className="text-center mt-12">
                   <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
-                    Jenna Stones
+                    Andra - Maria Năforniță
                   </h3>
                   <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
                     <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
-                    Los Angeles, California
-                  </div>
-                  <div className="mb-2 text-gray-700 mt-10">
-                    <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
-                    Solution Manager - Creative Tim Officer
-                  </div>
-                  <div className="mb-2 text-gray-700">
-                    <i className="fas fa-university mr-2 text-lg text-gray-500"></i>
-                    University of Computer Science
+                    Iași - România
                   </div>
                 </div>
                 <div className="mt-10 py-10 border-t border-gray-300 text-center">
+
                   <div className="flex flex-wrap justify-center">
+
                     <div className="w-full lg:w-9/12 px-4">
+                      <h1 style={{ fontSize: '30px' }}>Teacher</h1>
                       <p className="mb-4 text-lg leading-relaxed text-gray-800">
-                        An artist of considerable range, Jenna the name taken by
-                        Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                        performs and records all of his own music, giving it a
-                        warm, intimate feel with a solid groove structure. An
-                        artist of considerable range.
+                        The profession that creates all other professions.
                       </p>
                       <a
                         href="#pablo"
